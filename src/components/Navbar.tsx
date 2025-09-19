1a5a70b9caa9da12bc6fd509af2e0ead064f6203
allissonneo <allissonneo@gmail.com>
2025-09-19 16:44:29 -0300

    fix: navbar, sections, menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,33 @@
-function Navbar() {
-    return(
-        <nav>
-            <ul className="start">
-                <div className="itens start">
+import React, { useState } from "react";
+import "../styles/App.scss";
 
-                    <div className="logo">
-                        <li><a href="#sectionS"><img src="images/logo-branco.png" alt=""/></a></li>
-                    </div>
-                    <div className="item-nav">
-                        <li><a href="#section1"><strong>Workshops</strong></a></li>
-                    </div>
-                    <div className="item-nav">
-                        <li><a href="#section2"><strong>Competições</strong></a></li>
-                    </div>
-                    <div className="item-nav">
-                        <li><a href="#section3"><strong>Palestras</strong></a></li>
-                    </div>
-                    <div className="item-nav">
-                        <li><a href="#section4"><strong>Mostras Científicas</strong></a></li>
-                    </div>
-                    <div className="item-nav">
-                        <li><a href="#section4"><strong>Entretenimento</strong></a></li>
-                    </div>
-                </div>
-                <div className="item-nav-end start">
-                    <li><a href="#"><strong>Inscreva-se</strong></a></li>
-                </div>
-                
-            </ul>
-        </nav>
-    );
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <div className="logo">
+        <a href="#sectionS">
+          <img src="images/logo-branco.png" alt="Logo" />
+        </a>
+      </div>
+
+      <div className={`nav-links ${open ? "active" : ""}`}>
+        <a href="#section1"><p>Workshops</p></a>
+        <a href="#section2"><p>Competições</p></a>
+        <a href="#section3"><p>Palestras</p></a>
+        <a href="#section4"><p>Mostras Científicas</p></a>
+        <a href="#section5"><p>Entretenimento</p></a>
+        <a href="#"><p>Inscreva-se</p></a>
+      </div>
+
+    </nav>
+  );
 }
+
 export default Navbar;
