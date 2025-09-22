f63cd54e86f512808f3913e2f93aac13c6681d2f
Francisco Kaique <fkaiquesp@gmail.com>
2025-09-22 15:51:10 -0300

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,10 @@ function Footer() {
                 <p className="center">© 2025 - ValeTech.</p>
                 <div className="redes">
                     <div className="insta">
-                        <img src="images/extras/instagram.png" width="40px" alt="" />
+                        <img src="images/extras/instagram.png" alt="" />
                         <a className="a-color" href="https://www.instagram.com/valetech.aracati/">@valetech.aracati</a></div>
                     <div className="gmail">
-                        <img src="images/extras/gmail.png" width="40px" alt="" />
+                        <img src="images/extras/gmail.png" alt="" />
                         <a className="a-color" href="mailto:valetech.aracati@gmail.com">valetech.aracati@gmail.com</a></div>
                 </div>
             </div>
