bb5f9fc629703a96c6c6a1a29de91400508921f7
Francisco Kaique <fkaiquesp@gmail.com>
2025-09-30 16:57:56 -0300

    rm 'não perca'

--- src/components/SectionCenter.tsx
+++ src/components/SectionCenter.tsx
@@ -2,9 +2,8 @@ function SectionCenter() {
     return (
         <section className="section center section-down" id="sectionS">
             <div className="texto color-3 center">
-                <h1>Não Perca!!!</h1>
-                <strong><p className="inscreva-se">Inscrições Abertas</p>
-                <p className="inscreva-se">do dia 30 de setembro até 10 de outubro</p></strong>
+                <h1>Inscrições Abertas</h1>
+                <p className="inscreva-se">do dia 30 de setembro até 10 de outubro</p>
                 <button><h2><a href="https://www.even3.com.br/valetech-631126/">Inscrever-se</a></h2></button>
             </div>
         </section>
